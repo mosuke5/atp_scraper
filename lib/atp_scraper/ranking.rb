6f6b95215ed47649d76e224ec72868279a079c20
mosuke5 <w.vamos603@gmail.com>
2016-02-16 22:13:53 +0900

    Add rank and rank range in Raking.get

--- lib/atp_scraper/ranking.rb
+++ lib/atp_scraper/ranking.rb
@@ -10,10 +10,14 @@ module AtpScraper
   # Scrape ranking data
   class Ranking
     class << self
-      def get
-        request_uri = "/en/rankings/singles"
+      def get(rank_range = nil)
+        request_uri = build_uri(rank_range) 
         ranking_doc = AtpScraper::Html.get_and_parse(request_uri)
         pickup_ranking_data(ranking_doc)
+      end
+
+      def build_uri(rank_range = "0-100")
+        "/en/rankings/singles?rankRange=#{rank_range}"
       end
 
       def pickup_ranking_data(ranking_doc)
@@ -31,10 +35,15 @@ module AtpScraper
       def pickup_player_data(player_doc)
         url = pickup_player_url(player_doc)
         {
+          ranking: pickup_player_rank(player_doc),
           player_name: pickup_player_name(player_doc),
           player_url_name: get_url_name(url),
           player_id: get_url_id(url)
         }
+      end
+
+      def pickup_player_rank(player_doc)
+        player_doc.css(".rank-cell").first.content.strip
       end
 
       def pickup_player_name(player_doc)
