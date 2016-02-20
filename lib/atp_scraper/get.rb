5c1a2572a40352cd2b63437639c886bf5f2b7d0a
mosuke5 <w.vamos603@gmail.com>
2016-02-20 19:56:19 +0900

    Modify Get class

--- lib/atp_scraper/get.rb
+++ lib/atp_scraper/get.rb
@@ -3,16 +3,16 @@ module AtpScraper
   class Get
     def self.singles_ranking(rank_range = nil)
       request_uri = "/en/rankings/singles?rankRange=#{rank_range}"
-      ranking_doc = AtpScraper::Html.get_and_parse(request_uri)
-      ranking = AtpScraper::Ranking.new
-      ranking.pickup_ranking_data(ranking_doc)
+      ranking_html = AtpScraper::Html.get(request_uri)
+      ranking = AtpScraper::Ranking.new(ranking_html[:html], ranking_html[:charset])
+      ranking.pickup_ranking_data
     end
 
     def self.player_activity(player_id, year)
       request_uri = "/players/anything/#{player_id}/player-activity?year=#{year}"
-      activity_doc = AtpScraper::Html.get_and_parse(request_uri)
-      activity = AtpScraper::Activity.new
-      activity.pickup_activity_data(activity_doc)
+      activity_html = AtpScraper::Html.get(request_uri)
+      activity = AtpScraper::Activity.new(activity_html[:html], activity_html[:charset])
+      activity.pickup_activity_data
     end
   end
 end
